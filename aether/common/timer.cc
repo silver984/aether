4f276be1b69d47430b45321211a3e357cf2cb043
SILVERSTAR <silverstar.apollo@gmail.com>
2026-05-09 19:48:07 +0800

    use `steady_clock` instead of `high_resolution_clock`

--- aether/common/timer.cc
+++ aether/common/timer.cc
@@ -3,11 +3,11 @@
 namespace ae::timer {
 
 std::chrono::steady_clock::time_point start() {
-	return std::chrono::high_resolution_clock::now();
+	return std::chrono::steady_clock::now();
 }
 
 long long end(std::chrono::steady_clock::time_point const& start) {
-	std::chrono::steady_clock::time_point const now = std::chrono::high_resolution_clock::now();
+	std::chrono::steady_clock::time_point const now = std::chrono::steady_clock::now();
 	std::chrono::milliseconds const ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
 	return ms.count();
 }
