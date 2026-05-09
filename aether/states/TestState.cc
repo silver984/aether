ab8df90a2d719ee146f4f37e9d92c2673bd0f6ee
SILVERSTAR <silverstar.apollo@gmail.com>
2026-05-09 19:37:31 +0800

    macos please stop crying

--- aether/states/TestState.cc
+++ aether/states/TestState.cc
@@ -9,7 +9,13 @@ TestState::~TestState() = default;
 
 bool TestState::init(ae::Context const& ctx) {
 	if (auto renderer = ctx.renderer().lock()) {
-		renderer->set_background_rgba(ae::rgb(202ui8, 255ui8, 77ui8), 1.f);
+		renderer->set_background_rgba(
+			ae::rgb(
+				static_cast<uint8_t>(202),
+				static_cast<uint8_t>(255),
+				static_cast<uint8_t>(77)
+			), 1.f
+		);
 	}
 
 	if (auto grah = ae::Node::create<ae::Graphic>(ctx, "resources/grah.png")) {
