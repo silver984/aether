abdf92039a160ceeaeb15dcfb8bd874fdff635f4
SILVERSTAR <silverstar.apollo@gmail.com>
2026-06-03 17:00:29 +0800

    [`operators`] Use `std::common_type_t<...>`

--- include/math/operators.hh
+++ include/math/operators.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
+#include <functional>
 #include <math/numeric.hh>
+#include <type_traits>
 
 namespace aether {
 
@@ -15,157 +17,65 @@ struct operators {
 	}
 
 	[[nodiscard]] constexpr derived<T> operator-() const {
-		derived<T> out(self());
-
+		derived<T> out;
 		for (std::size_t i = 0; i < N; ++i) {
-			out[i] *= -1;
+			out[i] = -(self()[i]);
 		}
-
 		return out;
 	}
 
-	[[nodiscard]] constexpr derived<T> operator+(derived<T> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] += rhs[i];
-		}
-
-		return out;
+	template <indexed_numeric Rhs>
+	[[nodiscard]] constexpr auto operator+(Rhs const& rhs) const {
+		return binary(rhs, std::plus<>{});
 	}
 
 	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator+(derived<U> rhs) const {
-		derived<T> out(self());
+	[[nodiscard]] constexpr auto operator+(U rhs) const {
+		return binary(rhs, std::plus<>{});
+	}
 
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] += static_cast<T>(rhs[i]);
-		}
-
-		return out;
+	template <indexed_numeric Rhs>
+	[[nodiscard]] constexpr auto operator-(Rhs const& rhs) const {
+		return binary(rhs, std::minus<>{});
 	}
 
 	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator+(U rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] += static_cast<T>(rhs);
-		}
-
-		return out;
+	[[nodiscard]] constexpr auto operator-(U rhs) const {
+		return binary(rhs, std::minus<>{});
 	}
 
-	[[nodiscard]] constexpr derived<T> operator-(derived<T> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] -= rhs[i];
-		}
-
-		return out;
+	template <indexed_numeric Rhs>
+	[[nodiscard]] constexpr auto operator*(Rhs const& rhs) const {
+		return binary(rhs, std::multiplies<>{});
 	}
 
 	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator-(derived<U> rhs) const {
-		derived<T> out(self());
+	[[nodiscard]] constexpr auto operator*(U rhs) const {
+		return binary(rhs, std::multiplies<>{});
+	}
 
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] -= static_cast<T>(rhs[i]);
-		}
-
-		return out;
+	template <indexed_numeric Rhs>
+	[[nodiscard]] constexpr auto operator/(Rhs const& rhs) const {
+		return binary(rhs, std::divides<>{});
 	}
 
 	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator-(U rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] -= static_cast<T>(rhs);
-		}
-
-		return out;
+	[[nodiscard]] constexpr auto operator/(U rhs) const {
+		return binary(rhs, std::divides<>{});
 	}
 
-	[[nodiscard]] constexpr derived<T> operator*(derived<T> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] *= rhs[i];
-		}
-
-		return out;
-	}
-
-	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator*(derived<U> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] *= static_cast<T>(rhs[i]);
-		}
-
-		return out;
-	}
-
-	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator*(U rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] *= static_cast<T>(rhs);
-		}
-
-		return out;
-	}
-
-	[[nodiscard]] constexpr derived<T> operator/(derived<T> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] /= rhs[i];
-		}
-
-		return out;
-	}
-
-	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator/(derived<U> rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] /= static_cast<T>(rhs[i]);
-		}
-
-		return out;
-	}
-
-	template <numeric U>
-	[[nodiscard]] constexpr derived<T> operator/(U rhs) const {
-		derived<T> out(self());
-
-		for (std::size_t i = 0; i < N; ++i) {
-			out[i] /= static_cast<T>(rhs);
-		}
-
-		return out;
-	}
-
-	constexpr derived<T>& operator+=(derived<T> rhs) {
+	constexpr derived<T>& operator+=(derived<T> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] += rhs[i];
 		}
-
 		return self();
 	}
 
 	template <numeric U>
-	constexpr derived<T>& operator+=(derived<U> rhs) {
+	constexpr derived<T>& operator+=(derived<U> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] += static_cast<T>(rhs[i]);
 		}
-
 		return self();
 	}
 
@@ -174,24 +84,21 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] += static_cast<T>(rhs);
 		}
-
 		return self();
 	}
 
-	constexpr derived<T>& operator-=(derived<T> rhs) {
+	constexpr derived<T>& operator-=(derived<T> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] -= rhs[i];
 		}
-
 		return self();
 	}
 
 	template <numeric U>
-	constexpr derived<T>& operator-=(derived<U> rhs) {
+	constexpr derived<T>& operator-=(derived<U> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] -= static_cast<T>(rhs[i]);
 		}
-
 		return self();
 	}
 
@@ -200,24 +107,21 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] -= static_cast<T>(rhs);
 		}
-
 		return self();
 	}
 
-	constexpr derived<T>& operator*=(derived<T> rhs) {
+	constexpr derived<T>& operator*=(derived<T> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] *= rhs[i];
 		}
-
 		return self();
 	}
 
 	template <numeric U>
-	constexpr derived<T>& operator*=(derived<U> rhs) {
+	constexpr derived<T>& operator*=(derived<U> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] *= static_cast<T>(rhs[i]);
 		}
-
 		return self();
 	}
 
@@ -226,24 +130,21 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] *= static_cast<T>(rhs);
 		}
-
 		return self();
 	}
 
-	constexpr derived<T>& operator/=(derived<T> rhs) {
+	constexpr derived<T>& operator/=(derived<T> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] /= rhs[i];
 		}
-
 		return self();
 	}
 
 	template <numeric U>
-	constexpr derived<T>& operator/=(derived<U> rhs) {
+	constexpr derived<T>& operator/=(derived<U> const& rhs) {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] /= static_cast<T>(rhs[i]);
 		}
-
 		return self();
 	}
 
@@ -252,7 +153,6 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			self()[i] /= static_cast<T>(rhs);
 		}
-
 		return self();
 	}
 
@@ -260,7 +160,6 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			++self()[i];
 		}
-
 		return self();
 	}
 
@@ -274,7 +173,6 @@ struct operators {
 		for (std::size_t i = 0; i < N; ++i) {
 			--self()[i];
 		}
-
 		return self();
 	}
 
@@ -284,32 +182,47 @@ struct operators {
 		return out;
 	}
 
-	[[nodiscard]] constexpr bool operator==(derived<T> rhs) const {
+	[[nodiscard]] constexpr bool operator==(derived<T> const& rhs) const {
 		for (std::size_t i = 0; i < N; ++i) {
 			if (self()[i] != rhs[i]) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
-	[[nodiscard]] constexpr bool operator!=(derived<T> rhs) const {
+	[[nodiscard]] constexpr bool operator!=(derived<T> const& rhs) const {
 		return !(self() == rhs);
 	}
 
 	template <numeric U>
 	[[nodiscard]] explicit constexpr operator derived<U>() const {
 		derived<U> out;
-
 		for (std::size_t i = 0; i < N; ++i) {
 			out[i] = static_cast<U>(self()[i]);
 		}
-
 		return out;
 	}
 
 private:
+	template <indexed_numeric Rhs, typename F>
+	constexpr auto binary(Rhs const& rhs, F&& op) const -> derived<std::common_type_t<T, typename Rhs::value_type>> {
+		derived<std::common_type_t<T, typename Rhs::value_type>> out;
+		for (std::size_t i = 0; i < N; ++i) {
+			out[i] = op(self()[i], rhs[i]);
+		}
+		return out;
+	}
+
+	template <numeric U, typename F>
+	constexpr auto binary(U rhs, F&& op) const -> derived<std::common_type_t<T, U>> {
+		derived<std::common_type_t<T, U>> out;
+		for (std::size_t i = 0; i < N; ++i) {
+			out[i] = op(self()[i], rhs);
+		}
+		return out;
+	}
+
 	[[nodiscard]] constexpr derived<T> const& self() const {
 		return static_cast<derived<T> const&>(*this);
 	}
--- include/util/math.hh
+++ include/util/math.hh
@@ -17,18 +17,16 @@ struct size;
 namespace aether::util {
 
 template <numeric T>
-[[nodiscard]] constexpr T lerp(T const& a, T const& b, T const& t) {
+[[nodiscard]] constexpr T lerp(T a, T b, T t) {
 	return a + (b - a) * t;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T lerp(T const& a, T const& b, typename T::value_type const& t) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = a[i] + (b[i] - a[i]) * t;
 	}
-
 	return out;
 }
 
@@ -38,170 +36,147 @@ template <indexed_numeric T>
 }
 
 template <numeric T>
-[[nodiscard]] constexpr int sign(T const& val) {
+[[nodiscard]] constexpr int sign(T val) {
 	return (val > T{0}) - (val < T{0});
 }
 
 template <numeric T>
-[[nodiscard]] constexpr T avg(T const& a, T const& b) {
+[[nodiscard]] constexpr T avg(T a, T b) {
 	return (a + b) / T{2};
 }
 
 template <numeric T>
-[[nodiscard]] constexpr T map(T const& val, T const& in_min, T const& in_max, T const& out_min, T const& out_max) {
+[[nodiscard]] constexpr T map(T val, T in_min, T in_max, T out_min, T out_max) {
 	return out_min + (out_max - out_min) * ((val - in_min) / (in_max - in_min));
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T clamp(T const& val, T const& min_val, T const& max_val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::clamp(val[i], min_val[i], max_val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] T round(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::round(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T ceil(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::ceil(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] T floor(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::floor(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T abs(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::abs(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] T sin(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::sin(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] T cos(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::cos(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] T tan(T const& val) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::tan(val[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T min(T const& left, T const& right) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::min(left[i], right[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T max(T const& left, T const& right) {
 	T out;
-
 	for (std::size_t i = 0; i < T::capacity(); ++i) {
 		out[i] = std::max(left[i], right[i]);
 	}
-
 	return out;
 }
 
 template <indexed_numeric T>
 [[nodiscard]] constexpr T reverse(T const& val) {
 	T out;
-	std::size_t const capacity = T::capacity();
-
-	for (std::size_t i = 0; i < capacity; ++i) {
-		out[i] = val[capacity - i - 1];
+	for (std::size_t i = 0; i < T::capacity(); ++i) {
+		out[i] = val[T::capacity() - i - 1];
 	}
-
 	return out;
 }
 
 template <numeric T>
-[[nodiscard]] vec2<T> normalize(vec2<T> const& val) {
+[[nodiscard]] vec2<T> normalize(vec2<T> val) {
 	T const len = std::sqrt(val.x * val.x + val.y * val.y);
 	return len == T{0} ? vec2<T>() : vec2<T>(val.x / len, val.y / len);
 }
 
 template <numeric T>
-[[nodiscard]] T length(vec2<T> const& val) {
+[[nodiscard]] T length(vec2<T> val) {
 	return std::sqrt(val.x * val.x + val.y * val.y);
 }
 
 template <numeric T>
-[[nodiscard]] T distance(vec2<T> const& a, vec2<T> const& b) {
+[[nodiscard]] T distance(vec2<T> a, vec2<T> b) {
 	return std::sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
 }
 
 template <numeric T>
-[[nodiscard]] constexpr T dot(vec2<T> const& a, vec2<T> const& b) {
+[[nodiscard]] constexpr T dot(vec2<T> a, vec2<T> b) {
 	return a.x * b.x + a.y * b.y;
 }
 
 template <numeric T>
-[[nodiscard]] constexpr T degrees_to_radians(T const& deg) {
+[[nodiscard]] constexpr T degrees_to_radians(T deg) {
 	return deg * (std::numbers::pi_v<T> / T{180});
 }
 
 template <numeric T>
-[[nodiscard]] constexpr T radians_to_degrees(T const& deg) {
+[[nodiscard]] constexpr T radians_to_degrees(T deg) {
 	return deg * (T{180} / std::numbers::pi_v<T>);
 }
 
