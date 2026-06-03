f28d5c39ab2854cbf5c7c57f46a7c1baa076395d
SILVERSTAR <silverstar.apollo@gmail.com>
2026-06-03 20:18:15 +0800

    [raylib] Add more config changes

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -11,12 +11,28 @@ function(fetch dependency_name repo tag)
 endfunction()
 
 function(aether_fetch_dependencies target)
-    add_compile_definitions(SUPPORT_FILEFORMAT_JPG=1) # enable for raylib
-    add_compile_definitions(SUPPORT_MODULE_RAUDIO=0) # disable for raylib, use own miniaudio instead
-    add_compile_definitions(SUPPORT_TRACELOG=0) # disable for raylib, use own logging instead
-    add_compile_definitions(SUPPORT_SCREEN_CAPTURE=0) # disable for raylib, we don't want this
     fetch(raylib https://github.com/raysan5/raylib.git 6.0)
     fetch(fmt https://github.com/fmtlib/fmt.git 12.1.0)
+
+    target_compile_definitions(raylib
+        PRIVATE
+            SUPPORT_FILEFORMAT_JPG=1
+            SUPPORT_MODULE_RAUDIO=0
+            SUPPORT_TRACELOG=0
+            SUPPORT_SCREEN_CAPTURE=0
+            SUPPORT_CAMERA_SYSTEM=0
+            SUPPORT_GESTURES_SYSTEM=0
+            SUPPORT_RPRAND_GENERATOR=0
+            SUPPORT_WINMM_HIGHRES_TIMER=0
+            SUPPORT_CLIPBOARD_IMAGE=0
+            SUPPORT_FILEFORMAT_OBJ=0
+            SUPPORT_FILEFORMAT_MTL=0
+            SUPPORT_FILEFORMAT_IQM=0
+            SUPPORT_FILEFORMAT_GLTF=0
+            SUPPORT_FILEFORMAT_VOX=0
+            SUPPORT_FILEFORMAT_M3D=0
+            SUPPORT_MESH_GENERATION=0
+    )
     
     target_link_libraries(${target}
         PRIVATE
