f55451095d51a5c4294b75d161babca0997cdf04
SILVERSTAR <silverstar.apollo@gmail.com>
2026-06-20 23:54:54 +0800

    a

--- aether/lua/bindings/math.cc
+++ aether/lua/bindings/math.cc
@@ -1,48 +1,60 @@
 #include <algorithm>
+#include <functional>
 #include <lua/lua_binding.hh>
-#include <math/rect.hh>
-#include <math/size.hh>
 #include <math/vec2.hh>
 
 namespace {
 
-template <aether::indexed_numeric T, aether::numeric U>
-using ctor2 = sol::constructors<T(), T(U), T(U, U)>;
+template <aether::indexed_numeric T, typename U>
+sol::usertype<T>& binary(sol::usertype<T>& ut, sol::meta_function meta, U&& op) {
+	ut[meta] = sol::overload(
+	        [=](T left, T right) -> T {
+		        return op(left, right);
+	        },
+	        [=](T left, float right) -> T {
+		        return op(left, right);
+	        },
+	        [=](float left, T right) -> T {
+		        return op(right, left);
+	        });
+	return ut;
+}
+
+template <aether::indexed_numeric T>
+sol::usertype<T>& operators(sol::usertype<T>& ut) {
+	using enum sol::meta_function;
+
+	ut = binary(ut, addition, std::plus<>{});
+	ut = binary(ut, subtraction, std::minus<>{});
+	ut = binary(ut, multiplication, std::multiplies<>{});
+	ut = binary(ut, division, std::divides<>{});
+
+	ut[equal_to] = [](T left, T right) -> bool {
+		return left == right;
+	};
+	ut[index] = [](T obj, int index) -> float {
+		return obj[index - 1];
+	};
+	ut[new_index] = [](T& self, int index, float value) -> void {
+		self[index - 1] = value;
+	};
+	ut["capacity"] = []() -> int {
+		return (int)T::capacity();
+	};
+
+	return ut;
+}
 
 struct math : aether::lua_binding {
 	void bind(sol::state_view lua) override {
-		using vec2 = aether::vec2<double>;
-		bind_common(lua.new_usertype<vec2>("vec2", ctor2<vec2, double>(), "x", &vec2::x, "y", &vec2::y));
-	}
-
-	template <aether::indexed_numeric T>
-	void bind_common(sol::usertype<T>&& usertype) {
-		using enum sol::meta_function;
-#define META(meta, op)                                                                                                                     \
-	usertype[meta] = sol::overload(                                                                                                    \
-	        [](T const& left, T const& right) -> T {                                                                                   \
-		        return left op right;                                                                                              \
-	        },                                                                                                                         \
-	        [](T const& left, double right) -> T {                                                                                     \
-		        return left op right;                                                                                              \
-	        })
-		META(addition, +);
-		META(subtraction, -);
-		META(multiplication, *);
-		META(division, /);
-#undef META
-		usertype[equal_to] = [](T const& left, T const& right) -> bool {
-			return left == right;
-		};
-		usertype[index] = [](T const& container, int index) -> double {
-			return container[index - 1];
-		};
-		usertype[new_index] = [](T& container, int index, double value) -> void {
-			container[index - 1] = value;
-		};
-		usertype["capacity"] = []() -> int {
-			return (int)T::capacity();
-		};
+		{
+			using vec2             = aether::vec2<float>;
+			using constructors     = sol::constructors<vec2(), vec2(float), vec2(float, float)>;
+			sol::usertype<vec2> ut = lua.new_usertype<vec2>("vec2", constructors());
+			ut                     = operators(ut);
+			ut["x"]                = &vec2::x;
+			ut["y"]                = &vec2::y;
+		}
 	}
 };
 
